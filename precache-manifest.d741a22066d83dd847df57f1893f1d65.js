9e2ef5a0bddba9c08ff2a55fbc5dba7f2252116b
Abdalla Ramadan <abdalla.ramadan@stranger-games.com>
2021-07-13 20:01:31 +0300

    Updates

---
+++ precache-manifest.d741a22066d83dd847df57f1893f1d65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abd6e816d3b81053a6e8084cf5d2ed12",
+    "url": "/icotest/index.html"
+  },
+  {
+    "revision": "2e20bf4453c7b262b720",
+    "url": "/icotest/static/css/main.229b26cb.chunk.css"
+  },
+  {
+    "revision": "e25a159cdfdf496f1c7a",
+    "url": "/icotest/static/js/2.69f90d2c.chunk.js"
+  },
+  {
+    "revision": "2e20bf4453c7b262b720",
+    "url": "/icotest/static/js/main.267ad8f6.chunk.js"
+  },
+  {
+    "revision": "af63a28355bb1270141e",
+    "url": "/icotest/static/js/runtime-main.8f9ae580.js"
+  },
+  {
+    "revision": "45801c900bd8e0655c493615dad699f0",
+    "url": "/icotest/static/media/logo.45801c90.png"
+  }
+]);
