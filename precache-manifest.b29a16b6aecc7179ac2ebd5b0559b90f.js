4c9ef2bd4967a5585ce2c1e945a3865b2a2272c0
Abdalla Ramadan <abdalla.ramadan@stranger-games.com>
2021-08-06 16:24:20 +0300

    Updates

---
+++ precache-manifest.b29a16b6aecc7179ac2ebd5b0559b90f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70195ae7b661abb6f12124e275c7307d",
+    "url": "/icotest/index.html"
+  },
+  {
+    "revision": "05e0b66de53bd26d49a3",
+    "url": "/icotest/static/css/2.fe62fa80.chunk.css"
+  },
+  {
+    "revision": "a5f6d373684691a0a125",
+    "url": "/icotest/static/css/main.229b26cb.chunk.css"
+  },
+  {
+    "revision": "05e0b66de53bd26d49a3",
+    "url": "/icotest/static/js/2.9249988f.chunk.js"
+  },
+  {
+    "revision": "a5f6d373684691a0a125",
+    "url": "/icotest/static/js/main.e888d1ac.chunk.js"
+  },
+  {
+    "revision": "af63a28355bb1270141e",
+    "url": "/icotest/static/js/runtime-main.8f9ae580.js"
+  },
+  {
+    "revision": "45801c900bd8e0655c493615dad699f0",
+    "url": "/icotest/static/media/logo.45801c90.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/icotest/precache-manifest.d0eb91c96d059fe23f34651391f159a1.js"
+  "/icotest/precache-manifest.b29a16b6aecc7179ac2ebd5b0559b90f.js"
 );
 
 self.addEventListener('message', (event) => {
